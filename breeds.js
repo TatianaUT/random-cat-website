ff18dba31dc3dc844f45a49b664794152d91b0ce
TatianaUT <103016146+TatianaUT@users.noreply.github.com>
2025-09-08 01:19:40 -0700

    Add files via upload
    Updated CSS styling, HTML, JS

--- breeds.js
+++ breeds.js
@@ -2,23 +2,48 @@ const breedsContainer  = document.getElementById("breedsContainer");
 
 fetch("https://api.thecatapi.com/v1/breeds")
     .then(response => response.json())
-    .then(data  => {
+    .then(data => {
         data.forEach(breed => {
         const div = document.createElement("div");
         div.classList.add("breed");
 
-        div.innerHTML = `
-        <h2>${breed.name}</h2>
-        <p><strong>Contry:</strong> ${breed.origin}</p>
-        <p><strong>Intelligence:</strong> ${breed.intelligence} / 5</p>
-        <p>${breed.description}</p>
-        `;
-
+// Check either breed.image.url or fallback by reference_image_id
+        let imgUrl = null;
+        if (breed.image && breed.image.url) {
+            imgUrl = breed.image.url;
+        } else if (breed.reference_image_id) {
+            // Making an additional request to the API
+            //fetch(`https://api.thecatapi.com/v1/images/${breed.reference_image_id}`)
+             //   .then(res => res.json())
+             //   .then(imgData => {
+            imgUrl = `https://cdn2.thecatapi.com/images/${breed.reference_image_id}.jpg`;
+        }
+        // Rendering the card
+        renderBreed(breed, imgUrl, div);
         breedsContainer.appendChild(div);
+        });
+    })
+    .catch(error => {
+        breedsContainer.innerHTML = "❌ Error loading cat breeds 😿";
+        console.error("Download error:", error);
     });
-})
-.catch(error => {
-    breedsContainer.innerHTML = "Error loading cat breeds 😿";
-    console.error(error);
-});
+    
+    function renderBreed(breed, imgUrl, div) {
+        div.innerHTML = `
+            <h2>${breed.name}</h2>
+            ${
+                imgUrl 
+                ? `<img src="${imgUrl}" alt="${breed.name}" style="width:250px; border-radius:8px;">` 
+                : `<p> 🐾 No image available</p>`
+            }
+            <p><strong>Country:</strong> ${breed.origin}</p>
+            <p><strong>Intelligence:</strong> ${breed.intelligence} / 5</p>
+            <p>${breed.description}</p>
+            `;
+    }
+         
+
+        
+
+
 
